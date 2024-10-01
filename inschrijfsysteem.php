c76e30e9ed0f172df53c3a2cc37d9b58de477684
ranamardikhi <160498566+518997@users.noreply.github.com>
2024-10-01 10:08:38 +0200

    inschrijfsysteem

--- inschrijfsysteem.php
+++ inschrijfsysteem.php
@@ -4,30 +4,28 @@
         <title></title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="mainstyle.css" rel="stylesheet">
-        <link href="inschrijfsysteem.css" rel="stylesheet">
+        <link rel="stylesheet" href='style.css'>
     </head>
     <body>
     <?php
+
     
     ?>
     
      <form action="action_page.php" method="post">
      <div class="imgcontainer">
-     <img src="assets/login-avatar.png" alt="Avatar" class="avatar">
-      <h2>Log In Op Uw Account</h2>
+     <img src="assets/inschrijven.png" alt="Avatar" class="avatar">
+      <h2>Registreer voor het Kart 2024 toernooi</h2>
       </div>
 
      <div class="container">
-     <label for="email">E-mail:</label>
+     <label for="email">Name:</label>
      <input type="text" name="email" required>
 
-     <label for="psw">Wachtwoord:</label>
+     <label for="psw">E-mail:</label>
      <input type="password" name="psw" required>
      </div>
      </form>
-     <button type="submit" class='submit'>Login</button><br>
-     <button type="button" class="account">Maak Account</button><span>OR</span>
-     <button class="psw">Vergeet Wachtwoord</button>
+     <button type='register' class='register'>Registreren</button>
     </body>
     </html>
