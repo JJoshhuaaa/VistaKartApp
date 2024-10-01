816118a4edcf6405691371e6317e37d701c88997
ranamardikhi <160498566+518997@users.noreply.github.com>
2024-10-01 12:01:05 +0200

    Update inschrijfsysteem.php

--- inschrijfsysteem.php
+++ inschrijfsysteem.php
@@ -1,31 +1,43 @@
-<!DOCTYPE html>
+     <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/mainstyle.css" rel="stylesheet">
-        <link href="css/inschrijfsysteem.css" rel="stylesheet">
-    </head>
-    <body>
-    <?php
-    
-    ?>
-    
-     <form action="action_page.php" method="post">
-     <div class="imgcontainer">
-     <img src="assets/inschrijven.png" alt="Avatar" class="avatar">
-      <h2>Registreer voor het Kart 2024 toernooi</h2>
-      </div>
 
-     <div class="container">
-     <label for="email">Name:</label>
-     <input type="text" name="email" required>
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href='style.css'>
+</head>
+<body>
+     <?php
+     // Databaseverbinding
+     $host = 'localhost';
+     $db = 'vista toernooi';
+     $user = 'root';
+     $password = '';
+     $conn = new mysqli($host, $user, $password, $db);
 
-     <label for="psw">E-mail:</label>
-     <input type="password" name="psw" required>
-     </div>
-     </form>
-     <button type='register' class='register'>Registreren</button>
+        ?>
+        
+    <form action="action_page.php" method="post">
+        <div class="imgcontainer">
+            <img src="assets/inschrijven.png" alt="Avatar" class="avatar">
+            <h2>Registreer voor het Kart 2024 toernooi</h2>
+        </div>
+    
+        <div class="container">
+            <label for="email">Naam:</label>
+            <input type="text" name="email" required>
+    
+            <label for="email">E-mail:</label>
+            <input type="email" name="email" required>
+            <label for="psw">Wachtwoord:</label>
+            <input type="password" name="psw" required>
+        </div>
+    </form>
+    <button type='register' class='register'>Registreren</button>
     </body>
+    
     </html>
+    
+    
+     
