f337eaab7eafabfeab5312eaee43d8e4792a7018
ranamardikhi <160498566+518997@users.noreply.github.com>
2024-10-02 10:04:39 +0200

    config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+     // Databaseverbinding
+     $host = 'localhost';
+     $db = 'vista toernooi';
+     $user = 'root';
+     $password = '';
+     $conn = new mysqli($host, $user, $password, $db);
+?>
