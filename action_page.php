64199d1a311a020ce26695cbde5fde566a97d530
ranamardikhi <160498566+518997@users.noreply.github.com>
2024-10-03 13:30:25 +0200

    r

--- action_page.php
+++ action_page.php
@@ -1,41 +1,41 @@
-<?php
-// Verbinding maken met de database
+<?php 
 include 'config.php'; // Zorg ervoor dat je 'config.php' de juiste databaseverbinding heeft
 
 $error_message = "";
 
-
-
 // Controleren of het formulier is verzonden
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Gegevens ophalen uit het formulier
-    $name = mysqli_real_escape_string($link, $_POST['naam']); 
+    $naam = mysqli_real_escape_string($link, $_POST['naam']); 
     $email = mysqli_real_escape_string($link, $_POST['email']);
-    $password = mysqli_real_escape_string($link, $_POST['wachtwoord']); 
+    $wachtwoord = mysqli_real_escape_string($link, $_POST['wachtwoord']); 
 
+    // Controleer of het e-mailadres eindigt op '@vistacollege.nl'
     if (strpos($email, '@vistacollege.nl') !== false) {
-    
-    // Wachtwoord versleutelen
-    $hashed_password = password_hash($wachtwoord, PASSWORD_DEFAULT); 
+        // Wachtwoord versleutelen
+        $hashed_password = password_hash($wachtwoord, PASSWORD_DEFAULT); 
 
-    // SQL-query om gegevens in de database in te voegen
-    $sql = "INSERT INTO leerlingen (naam, email, wachtwoord) VALUES ('$naam', '$email', '$hashed_password')";
-    
-    // Uitvoeren van de query en controleren op succes
-    if (mysqli_query($link, $sql)) {
-        echo "Registratie succesvol!"; 
-        // Hier kun je een redirect doen naar bijvoorbeeld de prijzenpagina
-        header("Location: prijzen.html");
-        exit();
+        // SQL-query om gegevens in de database in te voegen
+        $sql = "INSERT INTO leerlingen (naam, email, wachtwoord) VALUES ('$naam', '$email', '$hashed_password')";
+        
+        // Uitvoeren van de query en controleren op succes
+        if (mysqli_query($link, $sql)) {
+            // Redirect naar de prijzenpagina na succesvolle registratie
+            header("Location: prijzen.html");
+            exit();
+        } else {
+            echo "Er is iets misgegaan: " . mysqli_error($link);
+        }
     } else {
-        echo "Er is iets misgegaan: " . mysqli_error($link);
+        // Redirect terug naar het formulier met een foutmelding
+        header("Location: inschrijfsysteem.php?error=Onjuiste e-mail, alleen Vista College e-mail toegestaan.");
+        exit();
     }
-}else{
-     echo "<p style='color:red;'>Onjuiste e-mail, alleen Vista College e-mail toegestaan.</p>";
-}
 
     // Sluiten van de databaseverbinding
     mysqli_close($link);
 }
 
+
 ?>
+
--- inschrijfsysteem.php
+++ inschrijfsysteem.php
@@ -1,44 +1,42 @@
-<?php 
-include 'config.php'
-?>
 <!DOCTYPE html>
 <html lang="nl">
 <head>
     <title>Vista Kart 2024 Toernooi</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel='stylesheet' href='css/inschrijfsysteem.css'>
+    <link rel='stylesheet' href='css/inschrijfsysteem.css'> 
 </head>
 <body>
-     <header class="header">
+    <header class="header">
         <div class="logo">
-            <img src="img/vistalogo.png"  alt="Karting Logo">
+            <img src="img/vistalogo.png" alt="Karting Logo">
         </div>
         <nav class="nav-menu">
             <a href="prijzen.html">Prijzen</a>
             <a href="#">Toernooi</a>
             <a href="contact.html">Contact</a>
-            <a href="inschrijfsysteem.php">Aanmelden</a>
+            <a href="inschrijfsysteem.html">Aanmelden</a>
         </nav>
     </header>
     <form action="action_page.php" method="post">
         <div class="imgcontainer">
-           <img src="assets/inschrijven.png" alt="Avatar" class="avatar">
+            <img src="assets/inschrijven.png" alt="Avatar" class="avatar">
             <h2>Registreer voor het Vista Kart 2024 toernooi</h2>
             <label for="name">Naam:</label>
             <input type="text" name="name" required>
-    
+
             <label for="email">E-mail:</label>
             <input type="email" name="email" required>
 
-            <?php if (!empty($error_message)): ?>
-            <p style='color:red;'><?php echo $error_message; ?></p>
+            <?php if (isset($_GET['error'])): ?>
+                <p style='color:red;'><?php echo htmlspecialchars($_GET['error']); ?></p>
             <?php endif; ?>
             
             <label for="psw">Wachtwoord:</label>
             <input type="password" name="psw" required>
         
-        <input type="submit" name="Submit" value="Registreren" class='submit'>
+            <input type="submit" name="Submit" value="Registreren" class='submit'>
+        </div>
     </form>
 </body>
 </html>
