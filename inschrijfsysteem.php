f46ad51fb31aacd5e0c3e5295a9c14e973121be9
ranamardikhi <160498566+518997@users.noreply.github.com>
2024-10-09 10:12:43 +0200

    i

--- inschrijfsysteem.php
+++ inschrijfsysteem.php
@@ -1,11 +1,7 @@
-<?php
-include 'lang.php'; // Inclusief de taalvariabelen
-?>
-
 <!DOCTYPE html>
-<html lang="<?php echo $lang; ?>">
+<html lang="nl">
 <head>
-    <title><?php echo $texts[$lang]['title']; ?></title>
+    <title>VISTA Kart App</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel='stylesheet' href='css/inschrijfsysteem.css'>
@@ -21,96 +17,29 @@ include 'lang.php'; // Inclusief de taalvariabelen
     <link rel="stylesheet" href="header-footer/header-footer.css">
 
     <form action="action_page.php" method="post">
-        <div class="imgcontainer">
-            <img src="assets/inschrijven.png" alt="Avatar" class="avatar">
-            <h2><?php echo $texts[$lang]['register']; ?></h2>
-
-            <label for="name"><?php echo $texts[$lang]['name']; ?></label>
+            <img src="assets/inschrijven.png" alt="avatar">
+            <h2>Schrijf in voor het Vista Kart 2024 toernoo</h2>
+            
+            <label for="name">Naam:</label>
             <input type="text" name="naam" required>
 
-            <label for="last name"><?php echo $texts[$lang]['lastname']; ?></label>
+            <label for="last name">Achternaam:</label>
             <input type="text" name="achternaam" required>
 
-            <label for="email"><?php echo $texts[$lang]['email']; ?></label>
+            <label for="email">E-mail:</label>
             <input type="email" name="email" required>
 
             <!-- Hier verschijnt de foutmelding als de e-mail onjuist is -->
             <?php if (isset($_GET['error']) && $_GET['error'] == 'invalid_email'): ?>
-                <p style="color:red; text-align:center;"><?php echo $texts[$lang]['invalid_email']; ?></p>
+             <p style="color:red; text-align:center;">Onjuiste e-mail.</p>
             <?php endif; ?>
             <!-- Hier verschijnt de foutmelding als de e-mail al bestaat -->
             <?php if (isset($_GET['error']) && $_GET['error'] == 'email_exists'): ?>
-                <p style="color:red; text-align:center;"><?php echo $texts[$lang]['email_exists']; ?></p>
+            <p style="color:red; text-align:center;">Dit e-mailadres is al geregistreerd.</p>
             <?php endif; ?>
 
-            <input type="submit" name="Submit" value="<?php echo $texts[$lang]['submit']; ?>" class='submit'>
-        </div>
-    </form>
-
-    <!-- Taalwisselknoppen -->
-    <div>
-        <a href="?lang=nl"><?php echo $texts[$lang]['change_lang']; ?> Nederlands</a> | 
-        <a href="?lang=en"><?php echo $texts[$lang]['change_lang']; ?> English</a>
-    </div>
-</body>
-</html>
-<?php
-include 'lang.php'; // Inclusief de taalvariabelen
-?>
-
-<!DOCTYPE html>
-<html lang="<?php echo $lang; ?>">
-<head>
-    <title><?php echo $texts[$lang]['title']; ?></title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel='stylesheet' href='css/inschrijfsysteem.css'>
-</head>
-<body>
-    <!--preload de plek voor de header-->
-    <div id="preloader"></div>
-    <!--hij laat de header dynamisch in-->
-    <div id="header-placeholder"></div>
-    <!--import header-footer js-->
-    <script src="./header-footer/header-footer.js"></script>
-    <!--import header-footer css-->
-    <link rel="stylesheet" href="header-footer/header-footer.css">
-
-    <form action="action_page.php" method="post">
-        <div class="imgcontainer">
-            <img src="assets/inschrijven.png" alt="Avatar" class="avatar">
-            <h2><?php echo $texts[$lang]['register']; ?></h2>
-
-            <label for="name"><?php echo $texts[$lang]['name']; ?></label>
-            <input type="text" name="naam" required>
-
-            <label for="last name"><?php echo $texts[$lang]['lastname']; ?></label>
-            <input type="text" name="achternaam" required>
-
-            <label for="email"><?php echo $texts[$lang]['email']; ?></label>
-            <input type="email" name="email" required>
-
-            <!-- Hier verschijnt de foutmelding als de e-mail onjuist is -->
-            <?php if (isset($_GET['error']) && $_GET['error'] == 'invalid_email'): ?>
-                <p style="color:red; text-align:center;"><?php echo $texts[$lang]['invalid_email']; ?></p>
-            <?php endif; ?>
-            <!-- Hier verschijnt de foutmelding als de e-mail al bestaat -->
-            <?php if (isset($_GET['error']) && $_GET['error'] == 'email_exists'): ?>
-                <p style="color:red; text-align:center;"><?php echo $texts[$lang]['email_exists']; ?></p>
-            <?php endif; ?>
-
-            <input type="submit" name="Submit" value="<?php echo $texts[$lang]['submit']; ?>" class='submit'>
-        </div>
-    </form>
-
-    <!-- Taalwisselknoppen -->
-    <div>
-        <a href="?lang=nl"><?php echo $texts[$lang]['change_lang']; ?> Nederlands</a> | 
-        <a href="?lang=en"><?php echo $texts[$lang]['change_lang']; ?> English</a>
-    </div>
-</body>
-</html>
-
+             <input type="submit" name="Submit" value="Inschrijven" class='submit'>
+           
+            </form>
 
     
-     
