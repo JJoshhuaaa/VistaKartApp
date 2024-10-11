982b98c6d9775c5c18bf4119405dfa484990e0a3
Elisei <144007909+Fl1ckJ@users.noreply.github.com>
2024-10-11 11:45:19 +0200

    prijzen translation+footer

--- prijzen.php
+++ prijzen.php
@@ -1,3 +1,4 @@
+<?php include 'lang_prijzen.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,72 +14,61 @@
     <title>Karting Prijzen</title>
 </head>
 <body>
-    <!--preload de plek voor de header-->
     <div id="preloader"></div>
-    <!--hij laat de header dynamisch in-->
     <div id="header-placeholder"></div>
-    <!--import header-footer js-->
     <script src="./header-footer/header-footer.js"></script>
-    <!--import header-footer css-->
     <link rel="stylesheet" href="header-footer/header-footer.css">
     
-    <br><a href="?lang=en" id='language' class='en'>EN</a>  |<a href="?lang=nl" id='language'>NL</a>
+    <br><a href="?lang=en" id='language' class='en'>EN</a>|<a href="?lang=nl" id='language' class="nl">NL</a>
     <section class="pricing-section">
-        <h1>Karting Prijzen</h1>
+        <h1><?php echo $language["heading"]; ?></h1>
         
         <div class="pricing-cards">
             <div class="card">
-                <h2>2e plaats</h2>
-                <img src="img/2nd.jpeg" loading="lazy" alt="Zilveren medaille">
-                <button data-modal="modal-2">Kijk naar de prijs</button>
+                <h2><?php echo $language["secondPlace"]; ?></h2>
+                <img src="img/2nd.png" loading="lazy" alt="Zilveren medaille">
+                <button data-modal="modal-2"><?php echo $language["viewPrize"]; ?></button>
             </div>
             <div class="card">
-                <h2>1e plaats</h2>
+                <h2><?php echo $language["firstPlace"]; ?></h2>
                 <img src="img/1st.png" loading="lazy" alt="Champion Cup">
-                <button data-modal="modal-1">Kijk naar de prijs</button>
+                <button data-modal="modal-1"><?php echo $language["viewPrize"]; ?></button>
             </div>
             <div class="card">
-                <h2>3e plaats</h2>
-
-                <img src="img/3d.jpg" loading="lazy" alt="Bronzen medaille">
-                <button data-modal="modal-3">Kijk naar de prijs</button>
+                <h2><?php echo $language["thirdPlace"]; ?></h2>
+                <img src="img/3rd.png" loading="lazy" alt="Bronzen medaille">
+                <button data-modal="modal-3"><?php echo $language["viewPrize"]; ?></button>
             </div>
         </div>
     </section>
-    
+    <?php include 'footer.php'; ?> 
 
+    <!-- Модальные окна для призов -->
     <div id="modal-1" class="modal">
         <div class="modal-content">
             <span class="close" data-modal="modal-1">&times;</span>
-            <img src="img/cup.png" loading="lazy" alt="">
-            <h2>Hoofdprijs: 1000 euro in contanten en de kampioensbeker</h2>
-            <p> De winnaar van het toernooi ontvangt de hoofdprijs van 1000 euro en de kampioensbeker, die uw overwinning en vaardigheden op de baan symboliseert. Deze beker wordt een blijvende herinnering en kan met trots worden getoond. Daarnaast krijgt de winnaar de mogelijkheid om deel te nemen aan VIP-karting evenementen in de toekomst. De prijs voor de eerste plaats is niet alleen een financiële beloning, maar ook een erkenning van uw vaardigheden en snelheid.</p>
+            <img src="img/1.png" loading="lazy" alt="">
+            <h2><?php echo $language["firstPrize"]; ?></h2>
+            <p><?php echo $language["firstPrizeText"]; ?></p>
         </div>
     </div>
     
     <div id="modal-2" class="modal">
         <div class="modal-content">
-            <img src="img/2nd.jpg" loading="lazy" alt="">
             <span class="close" data-modal="modal-2">&times;</span>
-            <h2>Prijs: 500 euro in contanten en een zilveren medaille</h2>
-            <p>De tweede plaats ontvangt 500 euro en een zilveren medaille. Deze prijs getuigt van uw uitstekende vaardigheden en uw verlangen om op hoog niveau te concurreren. De zilveren medaille is een teken dat u tot het uiterste heeft gestreden en opmerkelijke resultaten heeft behaald door tot de beste rijders van het toernooi te behoren. Deze prijs is een geweldige bevestiging van uw bekwaamheid en uitstekende vorm tijdens de competitie.</p>
+            <img src="img/2.png" loading="lazy" alt="">
+            <h2><?php echo $language["secondPrize"]; ?></h2>
+            <p><?php echo $language["secondPrizeText"]; ?></p>
         </div>
     </div>
     
     <div id="modal-3" class="modal">
         <div class="modal-content">
-            <img src="img/3.jpg" loading="lazy" alt="">
             <span class="close" data-modal="modal-3">&times;</span>
-            <h2>Prijs: 250 euro in contanten en een bronzen medaille.</h2>
-            <p>De prijs voor de derde plaats bestaat uit 250 euro en een bronzen medaille. Dit is een beloning voor degenen die indrukwekkende resultaten hebben laten zien tijdens het toernooi, waarbij ze moeilijke races en sterke tegenstanders hebben overwonnen. De bronzen medaille is een symbool van doorzettingsvermogen en het behalen van hoge resultaten, die u met trots aan uw vrienden en familie kunt laten zien. Deze prijs is een bewijs van uw buitengewone inspanningen en uw streven naar overwinning.</p>
+            <img src="img/3.png" loading="lazy" alt="">
+            <h2><?php echo $language["thirdPrize"]; ?></h2>
+            <p><?php echo $language["thirdPrizeText"]; ?></p>
         </div>
     </div>
-    
-    
-    <footer>
-        <p>© 2024 Vista Karting Area</p>
-    </footer>
 </body>
-<!--Nooit css of js in de html doen-->
-
 </html>
