9454e23661777ff26afb783e167d604c1b6672dd
/ </>
2024-10-14 11:19:34 +0200

    poules v1

--- adminpanel/rest/post.php
+++ adminpanel/rest/post.php
@@ -1,2 +1,9 @@
 <?php
-    
+    switch ($rest_path) {
+        case 'poule_add':
+            echo json_encode($link->query('SELECT * FROM leerlingen')->fetch_all(MYSQLI_ASSOC));
+            return;
+        default:
+            echo json_encode(array('error' => 'path not defined'));
+            return;
+    }
