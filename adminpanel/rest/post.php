1709d40ef581d660f9e7908a011815818b2ad1d9
/ </>
2024-10-14 12:25:12 +0200

    prep

--- adminpanel/rest/post.php
+++ adminpanel/rest/post.php
@@ -1,7 +1,13 @@
 <?php
+    $body = json_decode(file_get_contents('php://input'));
+
     switch ($rest_path) {
         case 'poule_add':
-            echo json_encode($link->query('SELECT * FROM leerlingen')->fetch_all(MYSQLI_ASSOC));
+            $statement = $link->prepare('SELECT * FROM poules WHERE naam=?');
+            $statement->bind_param('s', $body->naam);
+            $statement->execute();
+
+            echo json_encode($statement->get_result());
             return;
         default:
             echo json_encode(array('error' => 'path not defined'));
