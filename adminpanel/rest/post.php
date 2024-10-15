bb669fd7b31a258aab49e0a50eb248395f1936ba
/ </>
2024-10-15 12:23:02 +0200

    w

--- adminpanel/rest/post.php
+++ adminpanel/rest/post.php
@@ -2,12 +2,27 @@
     $body = json_decode(file_get_contents('php://input'));
 
     switch ($rest_path) {
-        case 'poule_add':
+        case 'poule_create':
             $statement = $link->prepare('SELECT * FROM poules WHERE naam=?');
-            $statement->bind_param('s', $body->naam);
+            $statement->bind_param('s',$body->naam);
             $statement->execute();
+            if(!empty($statement->get_result()->fetch_assoc()))
+                die(json_encode(array('error' => 'name already taken')));
 
-            echo json_encode($statement->get_result());
+            $statement = $link->prepare('INSERT INTO poules (naam) VALUES (?)');
+            $statement->bind_param('s', $body->naam);
+            
+            if($statement->execute())
+                echo json_encode($link->query('SELECT * FROM poules')->fetch_all(MYSQLI_ASSOC));
+            else echo json_encode(array('error' => 'unable to create poule'));
+
+            return;
+        case 'poule_update':
+            $statement = $link->prepare('UPDATE poules SET members=? WHERE id=?');
+            $statement->bind_param('si', $body->members, $body->id);
+            if($statement->execute())
+                echo json_encode($link->query('SELECT * FROM poules')->fetch_all(MYSQLI_ASSOC));
+            else echo json_encode(array('error' => 'unable to edit poule'));
             return;
         default:
             echo json_encode(array('error' => 'path not defined'));
